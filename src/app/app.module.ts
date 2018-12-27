2b872dd3aa44bb109cb12fd7a54ed97f58f0dc31
Jonathan <jpat82792@gmail.com>
2018-12-27 08:06:00 -0500

    Create routing logic, create dashboard/sign-up components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule, Routes} from '@angular/router';
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { TaskListRoutingModule } from './task-list-routing/task-list-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+  BrowserModule,
+  TaskListRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/task-list-routing/task-list-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AppComponent } from '../app.component';
+import { SignUpComponent } from '../sign-up/sign-up.component';
+import { DashboardComponent  } from '../dashboard/dashboard.component';
+
+const routes: Routes = [
+{path: 'sign-up', component: SignUpComponent},
+{path: 'dashboard', component: DashboardComponent},
+{path: '', redirectTo: '/sign-up', pathMatch: 'full'}
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class TaskListRoutingModule { }
