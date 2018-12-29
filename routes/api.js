a544de6da9e7aa489590e5ce0f0f43c8d7278432
Jonathan <jpat82792@gmail.com>
2018-12-29 17:35:49 -0500

    Add passport libraries, add routes for api, alter server.js for route

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
-
+const userController = require('../controllers/user-controller.js');
+const passport = require('passport');
+//const authController = require('../controllers/authentication-controller.js');
 /* GET api listing. */
 router.get('/', (req, res) => {
   res.send('api works');
@@ -8,6 +10,32 @@ router.get('/', (req, res) => {
 
 router.get('/hey', (req, res) => {
   res.send("No, good bye");
+});
+router.get('/login', (req, res)=>{
+  res.render('login.html',{});
+});
+router.post('/login', (req, res)=>{
+  const body = req.body;
+  passport.authenticate('local',(err, user, info)=>{
+    if(err){handleResponse(res, 500, 'error');}
+    if(!user){handleResponse(res, 400, 'bad req');}
+    if(user){res.redirect('');}
+  })(req, res);
+});
+router.get('/sign-up', (req, res) =>{
+  res.render('sign-up.html', {});
+});
+router.post('/user', (req, res) =>{
+  const body = req.body;
+  var result = userController.setUser(body.username, body.email, body.password);
+  result.then((e)=>{
+    console.log(e);
+    res.redirect('/login');
+  })
+  .catch((e)=>{
+    console.log(e);
+    res.redirect('/sign-up');
+  });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,16 +7,11 @@ const bodyParser = require('body-parser');
 // Get our API routes
 const api = require('./routes/api');
 const app = express();
-const router = require('./routes/router.js');
+const passport = require('./controllers/authenticator-controller.js')(app);
+//const router = require('./routes/router.js');
 // Parsers for POST data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-//Route roots
-app.use('/api',api);
-//app.use('/', router);
-/*app.get('/', (req, res) =>{
-  res.sendFile(path.join(__dirname, 'dist/task-list/index.html'));
-});*/
 
 //Configure engine to use ejs
 app.engine('html', require('ejs').renderFile);
@@ -24,10 +19,23 @@ app.set('view engine', 'html');
 // Point static path to dist
 app.use('/', express.static(path.join(__dirname, 'views')));
 
-app.use('/', express.static(path.join(__dirname, 'dist/task-list')));
-app.use(function(req, res, next){
-  res.sendFile(path.join(__dirname, 'dist/task-list/index.html'));
+//app.use('/', express.static(path.join(__dirname, 'dist/task-list')));
+app.use('/api',api);
+app.use('*',function(req, res, next){
+ console.log("HELLO!!!!");
+  if(req.isAuthenticated())
+  {
+    console.log("req.isauthenti");
+    console.log(req.isAuthenticated());
+    res.sendFile(path.join(__dirname, 'dist/task-list/index.html'));
+  }
+  else{
+    console.log("Req not auth");
+    res.redirect('/api/login');
+  }
 });
+app.use('/', express.static(path.join(__dirname, 'dist/task-list')));
+
 /**
  * Get port from environment and store in Express.
  */
