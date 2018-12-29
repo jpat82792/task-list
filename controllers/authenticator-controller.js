03e48679e13eaaefe96fcc679ee5ec2635c434d3
Jonathan <jpat82792@gmail.com>
2018-12-29 17:38:43 -0500

    Setup passport and routes to support login
    NOTE: login still does not work. Most likely a misconfiguration
    of passportjs

---
+++ controllers/authenticator-controller.js
@@ -0,0 +1,51 @@
+const passport = require('passport'),
+      LocalStrategy = require('passport-local').Strategy;
+const user = require('./user-controller.js');
+const isAuth = async function(username, password){
+  return user.getUser(username, username, password);
+}
+
+module.exports = function(app)
+{
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  passport.use(new LocalStrategy(
+        function(username, password, done){
+          var result = isAuth(username, password);
+          result.then((e)=>{
+            console.log(e);
+            if(e.length ===1)
+            {
+              console.log("user exist");
+              return done(null, e[0]);
+            }
+            else{
+              console.log("no user");
+              return done(null, false, {message: 'Incorrect credentials.'});
+            }
+          })
+          .catch((e)=>{
+            console.log(e);
+            return done(null, false, {message: 'Incorrect credentials.'});
+          });
+        }));
+  passport.serializeUser(function(user, done){
+    console.log("serialize");
+    console.log(user);
+    done(null, user[0].user_id);
+  });
+  passport.deserializeUser(function(id, done){
+    user.getUserById(id)
+      .then((e) =>{
+        console.log("deser");
+        console.log(e);
+        if(e === null)
+        {
+          
+          done(new Error('Wrong user id'));
+        }
+        done(null, user);
+      });
+  });
+}
---
+++ controllers/user-controller.js
@@ -0,0 +1,30 @@
+const pgp = require('pg-promise')();
+const connString = 'postgres://task_list_admin:tlIsBest21@172.17.0.2:5432/task_list';
+
+const db = pgp(connString);
+const setUserQuery = "INSERT INTO users(username, email, password) VALUES($(username), $(email), crypt($(password), gen_salt('bf')) ) RETURNING username;"
+const getUserQuery = "SELECT * from users where username = $(username) OR email=$(username) AND password = crypt($(password), gen_salt('bf'));";
+const getUserByIdQuery = "SELECT * FROM users where user_id=$(id)";
+const setUser = async function(username, email, password)
+{
+  console.log("setUser()");
+  const values = {"username":username, "email": email, "password": password};
+  var result = db.query(setUserQuery, values);
+  return result;
+};
+
+const getUser = async function(username, email, password)
+{
+  console.log("getUser()");
+  const parameters = {"username": username,  
+    "password": password};
+  var user = db.query(getUserQuery, parameters);
+  return user;
+}
+const getUserById = async function(id)
+{
+  const parameters = {"id": id};
+  return db.query(getUserByIdQuery,parameters );
+}
+
+module.exports = {"setUser":setUser, "getUser": getUser};
